e091933eb2fe73d114ade110b144b358d8433ffc
uran224 <uran22@gmail.com>
2020-05-24 12:04:08 +0300

    Lesson 34. Вынос мозга (FLUX-круговорот на каждый символ)
    Каждое изменение в поле input мы переносим в state и только после отображаем это изменение в UI

--- src/App.js
+++ src/App.js
@@ -25,8 +25,10 @@ const App = (props) => {
           render={() => <Dialogs state={props.state.dialogsPage} />} />
         <Route path="/profile"
           render={() => <Profile 
-          state={props.state.profilePage} 
-          addPost={props.addPost} />} />
+            profilePage={props.state.profilePage} 
+            addPost={props.addPost} 
+            updateNewPostText={ props.updateNewPostText }  
+          />} />
 
 
 
